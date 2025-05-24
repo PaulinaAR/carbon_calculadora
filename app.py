26e9d80f18561064cb2686ca290043a796b2d922
PaulinaAR <paulinaarreguinruiz108@gmail.com>
2025-05-23 21:43:50 -0600

    Update app.py 2.0

--- app.py
+++ app.py
@@ -182,4 +182,5 @@ def test_db():
         return f"❌ Error en la conexión a la base de datos: {e}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
